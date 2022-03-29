f6d2ff2eb0ab24f4357a2f3e981fcc829c245d1a
Sullkis <78040027+Sullkis@users.noreply.github.com>
2022-03-29 16:56:45 +0100

    ls

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,21 +2,24 @@ import React from 'react'
 
 const LoginForm = () => {
     return (
-        <div>
+        <div className="form-container">
             <form>
-                <div className="input-container">
-                    <label>Email: </label>
-                    <input type="email" name="email" required />
-                    {/* {renderErrorMessage("uname")} */}
+                <div className="form">
+                    <div className="input-container">
+                        <input type="email" name="email" required />
+                        <span></span>
+                        <label>Email</label>
+                    </div>
+                    <div className="input-container">
+                        <input type="password" name="pass" required />
+                        <span></span>
+                        <label>Password: </label>
+                    </div>
+                    <div className="button-container">
+                        <input className="submit-button" type="submit" value="Login" />
+                    </div>
                 </div>
-                <div className="input-container">
-                    <label>Password: </label>
-                    <input type="password" name="pass" required />
-                    {/* {renderErrorMessage("pass")} */}
-                </div>
-                <div className="button-container">
-                    <input type="submit" value="Login" />
-                </div>
+
             </form>
         </div>
     )
