c0402caabf4929cbce97b8933d9cfd8f7802201e
Sullkis <78040027+Sullkis@users.noreply.github.com>
2022-03-30 14:54:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './index.css';
 import LoginPage from './components/LoginPage';
-
+import { useState } from 'react';
+import Registry from "./Classes/Registry.js";
 
 function App() {
+
+  const[registry] = useState(new Registry());
+
   return (
+    
     <div className="app">
       <LoginPage />
     </div>
