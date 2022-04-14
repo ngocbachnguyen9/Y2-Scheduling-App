cc2a6580c6fa91c64c628270ab91673d8db2109b
Shyam9538 <77747232+Shyam9538@users.noreply.github.com>
2022-04-14 03:30:06 +0100

    v

--- src/Components/StaffDash.js
+++ src/Components/StaffDash.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Courses from "./Courses";
 
 
-function StaffDash({su, ud, Logout, cd, courseDelete, addCourse, addModule} ) {
+function StaffDash({ud, Logout, cd, courseDelete, addCourse, addModule} ) {
 
   const [showAddCourse, setShowAddCourse] = useState(false)
 
@@ -26,36 +26,32 @@ function StaffDash({su, ud, Logout, cd, courseDelete, addCourse, addModule} ) {
     <div className="StaffDash">
       {(ud.userType === "staff") ? (
         <div>
-          
-         <div className="trainer-dash-box-smaller">
-          <button className="btn btn-pos2" >Create Course</button>
+        <div className="trainer-dash-box-smaller">
         <button className="btn btn-pos2" >Create Course</button>
-          </div>
-    
-      <div className="trainer-dash-box">
+        <button className="btn btn-pos2" >Create Course</button>
+        </div>
+        <div className="trainer-dash-box">
           <div className="mainHeader">
-          <h1 className="title">Welcome, {ud.name}</h1>
+          <h1 className="title title-align">Welcome {ud.name}</h1>
           <button className="btn" style={{backgroundColor:"#4427fa"}} onClick={Logout}>Logout</button>
           </div>
-
           <div className="sdHeader">
-            <h2>Courses</h2>          
+            <h2 className="title-align">Courses</h2>          
             <button className="btn" onClick={addCourseView}>Create Course</button>
           </div>
-
           {showAddCourse && <form onSubmit={courseAddHandler}>
             <div className="course-form-container">
-              <label className="form-label1">Course Name </label>
-              <input className='input-style' type="text" name="courseName" id="course-name" required/>
+              <label className="form-label1">Course Name: </label>
+              <br></br>
+              <input className='input-style' placeholder='Add Course Name' type="text" name="courseName" id="course-name" required/>
             </div>
-           
             <div className="submit-container">
               <input className="btn btn-pos1 btn-course" type="submit" value="Add Course" />
             </div>
-           
+            <br></br>
           </form>}
-          <Courses su ={su} courses={cd} onDelete={courseDelete} addModule={addModule}/>
-        </div>
+          <Courses courses={cd} onDelete={courseDelete} addModule={addModule}/>
+          </div>
         </div>
       ) : (
         <div>
