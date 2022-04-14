6f4abc535416b9c3abfb55e429804797c5c38c29
Sullkis <78040027+Sullkis@users.noreply.github.com>
2022-04-14 04:04:55 +0100

    changes

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,118 +1,12 @@
 import LoginForm from "./LoginForm";
 import fdmLogo from '../assets/fdm_Logo2.png';
 import Particles from "react-tsparticles";
+import Par from "./Par";
 
 function Welcome({users,Login}){
     return(
             <div className="loginPageStuff">
-          <Particles
-            options={{
-                "particles": {
-                    "number": {
-                      "value": 320,
-                      "density": {
-                        "enable": true,
-                        "value_area": 800
-                      }
-                    },
-                    "color": {
-                      "value": "#ffffff"
-                    },
-                    "shape": {
-                      "type": "polygon",
-                      "stroke": {
-                        "width": 0,
-                        "color": "#000000"
-                      },
-                      "polygon": {
-                        "nb_sides": 5
-                      },
-                      "image": {
-                        "src": "img/github.svg",
-                        "width": 100,
-                        "height": 100
-                      }
-                    },
-                    "opacity": {
-                      "value": 0.49,
-                      "random": false,
-                      "anim": {
-                        "enable": true,
-                        "speed": 0.2,
-                        "opacity_min": 0,
-                        "sync": false
-                      }
-                    },
-                    "size": {
-                      "value": 2,
-                      "random": true,
-                      "anim": {
-                        "enable": true,
-                        "speed": 2,
-                        "size_min": 0,
-                        "sync": false
-                      }
-                    },
-                    "line_linked": {
-                      "enable": false,
-                      "distance": 150
-                    },
-                    "move": {
-                      "enable": true,
-                      "speed": 0.2,
-                      "random": true,
-                      "out_mode": "out",
-                      "bounce": false,
-                      "attract": {
-                        "enable": false,
-                        "rotateX": 600,
-                        "rotateY": 1200
-                      }
-                    }
-                  },
-                  "interactivity": {
-                    "detect_on": "canvas",
-                    "events": {
-                      "onhover": {
-                        "enable": true,
-                        "mode": "bubble"
-                      },
-                      "onclick": {
-                        "enable": true,
-                        "mode": "push"
-                      },
-                      "resize": true
-                    },
-                    "modes": {
-                      "grab": {
-                        "distance": 400,
-                        "line_linked": {
-                          "opacity": 1
-                        }
-                      },
-                      "bubble": {
-                        "distance": 83.91608391608392,
-                        "size": 1,
-                        "duration": 3,
-                        "opacity": 1,
-                        "speed": 3
-                      },
-                      "repulse": {
-                        "distance": 200,
-                        "duration": 0.4
-                      },
-                      "push": {
-                        "particles_nb": 4
-                      },
-                      "remove": {
-                        "particles_nb": 2
-                      }
-                    }
-                  },
-                  "retina_detect": true
-                }
-            }
-            />
+              <Par />
           <div className="logoSec">
             <img className="logo" src={fdmLogo} alt="" />
             <h1>Training and Scheduling</h1>
